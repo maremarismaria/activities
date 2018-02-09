8c9cdadc0a00044cb76475d78708d7ad15b4580b
Maria <mariamoralespadron@gmail.com>
2018-02-09 18:27:26 +0000

    script done from scratch

--- DAW/DEW/project/script.js
+++ DAW/DEW/project/script.js
@@ -1,83 +1,104 @@
 
 $().ready(function(){
     
-    var numRows = 3;
-    var numCols = 4;
-    makeScenario(numRows, numCols);
+    var level = [
+        [{life: 0},{life: 0},{life: 0},{life: 0}], //this line with life: 0 means empty space
+        [{life: 1},{life: 1},{life: 1},{life: 1}],
+        [{life: 2},{life: 2},{life: 2},{life: 2}],
+        [{life: 3},{life: 3},{life: 3},{life: 3}],
+    ];
 
+    makeScenario(level);
 
     //scenario properties
-    var scenarioPosition = $('#scenario').position();
     var scenarioWidth = $('#scenario').width();
     var scenarioHeight = $('#scenario').height();
 
     //ball properties
-    var ballMovement = 21;
-    var ballPosition = $('#ball').position();
     var ballWidth = $('#ball').width();
-    var ballHeight = $('#ball').height();
-    var ballX = $('#scenario').width() / 2;
-    var ballY = $('#scenario').height() - 20;
+    var ballX = scenarioWidth / 2;
+    var ballY = scenarioHeight - 40;
     var movX = 1;
     var movY = -1;
     
     //paddle properties
-    var paddlePosition = $('#paddle').position();
     var paddleHeight = $('#paddle').height();
     var paddleWidth = $('#paddle').width();
+    var paddleX = scenarioWidth / 2;
+    var paddleY = scenarioHeight;
+    $('#paddle').css( {"left" : paddleX} );
 
     //paddle movement
     $(document).on('keydown', function(event){           
         if(event.which == 37){ //<-
-            $('#paddle').css( {"left" : "-=10"} );
+            paddleX -= 10;
+            $('#paddle').css( {"left" : paddleX} );
         }
 
         if(event.which == 39){ //->
-            $('#paddle').css( {"left" : "+=10"} );
+            paddleX += 10;
+            $('#paddle').css( {"left" : paddleX} );
         }
     });
-    
-    var columns = document.getElementsByClassName('column');
-
-    for (var i = 0; i < columns.length; i++) {
-        var column = columns[i];
-        var columnId = "#" + column.id;
-        var columnX = $(columnId).position().left;
-        var columnY = $(columnId).position().top;
-        var columnWidth = $(columnId).width();
-        var columnHeight = $(columnId).height();
-        console.log(columnX + " - " + columnY + " / " + columnHeight + " - " + columnWidth);
-    }
-
 
     setInterval(function(){
 
         $('#ball').css( { "left" : ballX , "top" : ballY } );
 
-        if (ballX + movX > $('#scenario').width() - ballWidth){
-            $('#ball').css( { "background-color" : "red" } );
-            movX = -movX;
+        //top
+        if(ballY < 0){ 
+            $('#ball').css( { "background-color" : "yellow" } );
+            movY = -movY;
         }
 
-        if(ballX + movX < 0){
-            $('#ball').css( { "background-color" : "green" } );
-            movX = -movX;
-        }
-
-        if (ballY + movY > $('#scenario').height() - ballWidth){
+        //bottom
+        if (ballY > scenarioHeight - ballWidth){ 
             $('#ball').css( { "background-color" : "blue" } );
             console.log("GAME OVER");
             movY = -movY;
         }
 
-        if (ballY + movY > $('#scenario').height() - ballWidth - paddleHeight){
-            alert("ay");
+        //left
+        if(ballX < 0){
+            $('#ball').css( { "background-color" : "green" } ); 
+            movX = -movX;
         }
 
-        if(ballY + movY < ballWidth){
+        //right
+        if (ballX > scenarioWidth - ballWidth){ 
             $('#ball').css( { "background-color" : "red" } );
+            movX = -movX;
+        }
+
+        //paddle colission
+        if (ballX > paddleX && ballX < paddleX + paddleWidth && ballY == scenarioHeight - paddleHeight - ballWidth){ 
+            $('#ball').css( { "background-color" : "blue" } );
             movY = -movY;
         }
+        
+        //column colission
+        $('.row').each(function() {
+
+            var row = this.getBoundingClientRect();
+            
+
+            if(ballY > row.top && ballY < row.bottom){
+                
+                $(this).children('.column').each(function(){
+
+                    var column = this.getBoundingClientRect();
+
+                    if(ballY > column.top && ballY < column.bottom && ballX > column.left && ballX < column.right){
+                        
+                        alert("hey");
+
+                    }
+
+                });
+
+            }
+
+        });
 
         ballX += movX;
         ballY += movY;
@@ -88,19 +109,19 @@ $().ready(function(){
 
 
 
-function makeScenario(numRows, numCols){
+function makeScenario(level){
     
-    //add stage
-
-    for (var i = 0; i < numRows; i++) {
+    for (var i = 0; i < level.length; i++) {
         var currentRow = 'row-' + i;
+
         $('#scenario').append("<div class='row' id=" + currentRow + ">");
         
-        for (var j = 0; j < numCols; j++) {
+        for (var j = 0; j < level[i].length; j++) {
 
             var currentCol = 'col-' + j;
-            var life = Math.floor((Math.random() * 3) + 1);
-            $('#' + currentRow).append("<div class='column' id=" + currentCol + " data-life=" + life + ">" + life + "</div>");
+
+            $('#' + currentRow).append("<div class='column' id=" + currentCol + " data-life=" + level[i][j].life + ">" + level[i][j].life + "</div>");
+            
         }        
     }
 
